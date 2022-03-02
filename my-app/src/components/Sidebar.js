166d867bb286b7a8d5ef1f5695dfe4762345c061
oisintohak <oisintohak@gmail.com>
2022-03-02 21:26:14 +0000

    add navbar class based stateful component

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -3,9 +3,9 @@ import css from './css/Sidebar.module.css';
 const Sidebar = () => {
     return (
         <div className={css.sidebar}>
-            <a>My Photos</a>
-            <a>My Illustrations</a>
-            <a>My Paintings</a>
+            <a href='/'>My Photos</a>
+            <a href='/'>My Illustrations</a>
+            <a href='/'>My Paintings</a>
         </div>
     )
 }

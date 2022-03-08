8b323a6138102d7b79bf2dde3fc8d4306e2f3b20
oisintohak <oisintohak@gmail.com>
2022-03-08 15:02:55 +0000

    add Loader component with lifecycle method

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
 import css from './css/Content.module.css'
-import {savedPosts} from '../posts.json'
+import { savedPosts } from '../posts.json'
 import PostItem from './PostItem'
+import Loader from './Loader'
 
 export class Content extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            isLoaded: false,
+        }
     }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState(() => {
+                return {
+                    isLoaded: true,
+                }
+            })
+        }, 2000)
+    }
+
     render() {
         return (
             <div className={css.Content}>
@@ -14,7 +30,11 @@ export class Content extends Component {
                     <h1>My posts</h1>
                 </div>
                 <div className={css.SearchResults}>
-                    <PostItem savedPosts={savedPosts} />
+                    {this.state.isLoaded ?
+                        <PostItem savedPosts={savedPosts} />
+                        :
+                        <Loader />
+                    }
                 </div>
             </div>
         )
--- my-app/src/components/NavBarChild.js
+++ my-app/src/components/NavBarChild.js
@@ -13,3 +13,4 @@ export default function NavBarChild(props) {
         </div>
     )
 }
+
